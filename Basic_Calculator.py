d7737b2fd90561a8e05e86bc3fa3023f5d1dcf9a
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2021-03-01 14:28:21 +0000

    Add files via upload

---
+++ Basic_Calculator.py
@@ -0,0 +1,33 @@
+#this is a recap of what I have learnt in python as of 1st March 2021. I am writing this at 1:38 in the afternoon.
+#the math is not relevant.
+from math import *
+
+print("Welcome to this addition calculator!")
+
+name = input("Enter your name:" )
+
+age = input("Enter your age:" )
+
+num1 = input("Enter your first number:" )
+
+num2 = input("Enter your second number:" )
+
+result = (float(num1) + float(num2))
+
+
+
+print("Hello, " + name + "!")
+
+
+
+print("You are " + age + " years old")
+
+
+
+print("And this is the answer to your addition:")
+
+print(result)
+
+
+
+
